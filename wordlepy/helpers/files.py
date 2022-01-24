552e0efde796497f093bf5a7df3a58c3c196f677
Richard Allen <richard.allen@palaeo.eu>
2022-01-24 14:40:01 +0000

    WIP: include, exclude, pattern

--- wordlepy/helpers/files.py
+++ wordlepy/helpers/files.py
@@ -1,6 +1,11 @@
 import json
+import re
 
-from typing import Dict, Set
+from typing import Dict, List, Optional, Pattern, Set
+
+
+class PatternLengthException(Exception):
+    pass
 
 
 def parse_json(file_path: str) -> Dict:
@@ -20,6 +25,65 @@ def parse_json(file_path: str) -> Dict:
     return english_dictionary
 
 
+def _present(word: str, p_chars: List) -> Optional[str]:
+    matches: List = []
+
+    for char in p_chars:
+        if char in word:
+            matches.append(True)
+        else:
+            matches.append(False)
+
+    if all(matches):
+        return word
+
+    return None
+
+
+def _absent(word: str, a_chars: List) -> Optional[str]:
+    matches: List = []
+
+    for char in a_chars:
+        if char not in word:
+            matches.append(True)
+        else:
+            matches.append(False)
+
+    if all(matches):
+        return word
+
+    return None
+
+
+def _build_regex(raw_pattern: str, length: int = 5, present: bool = True) -> Pattern:
+    if len(raw_pattern) > length:
+        raise PatternLengthException("Raw pattern used is greater than the set length.")
+
+    pattern: str = r"^"
+
+    if not raw_pattern:
+        return re.compile(r"^[\w][\w][\w][\w][\w]")
+
+    for char in raw_pattern:
+        if char == "?":
+            pattern = pattern + "[\w]"
+        if present and char.isalpha():
+            pattern = pattern + f"[{char}]"
+        if not present and char.isalpha():
+            pattern = pattern + f"[^{char}]"
+
+    regex = re.compile(pattern)
+    return regex
+
+
+def exclude_positions() -> Optional[str]:
+    pass
+
+
+def include_positions() -> Optional[str]:
+    pass
+
+
 def filter_entries(dictionary_subset: Set, **kwargs) -> Set:
     """
 
@@ -27,9 +91,19 @@ def filter_entries(dictionary_subset: Set, **kwargs) -> Set:
     :param kwargs:
     :return:
     """
+    c_present: List = kwargs.get("present")
+    c_absent: List = kwargs.get("absent")
+    # length: int = kwargs.get("length") or 5
+
+    filtered_results: Set = set()
+
+    for word in dictionary_subset:
+        if _present(word, c_present) and _absent(word, c_absent):
+            filtered_results.add(word)
+
     # example kwargs could be: contains, exclude, pattern_match, length, good_starters
     # research some good starter patterns, like least number of guesses that eliminate most words?
-    pass
+    return filtered_results
 
 
 def main():
@@ -37,7 +111,9 @@ def main():
 
     words_with_five_letters: Set = {key for key in raw_dictionary.keys() if len(key) == 5 and " " not in key}
 
-    print(words_with_five_letters)
+    filtered_words: Set = filter_entries(words_with_five_letters, present=["n", "k", "l"], absent=["f", "u", "d", "r", "a", "i", "e", "c", "s"])
+
+    print(filtered_words)
 
 
 if __name__ == "__main__":
